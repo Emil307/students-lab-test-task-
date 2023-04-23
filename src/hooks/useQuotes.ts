1c57254a54c09ceee4742f210df92f950924520a
Emil307 <emilnovikov392@gmail.com>
2023-04-23 16:56:25 +0600

    add loading add error add hooks for fetching data

---
+++ src/hooks/useQuotes.ts
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import { IQuotesList } from '../types/types';
+import axios, { AxiosError } from 'axios';
+
+export function useQuotes() {
+  const [quotes, setQuotes] = useState<IQuotesList[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const API = 'https://api.quotable.io';
+
+  async function getQuotes() {
+    try {
+      setError('');
+      setLoading(true);
+      const response = await axios.get<IQuotesList[]>(`${API}/quotes`);
+      setQuotes(response.data.results);
+      setLoading(false);
+    } catch (e: unknown) {
+      const error = e as AxiosError
+      setLoading(false);
+      setError(error.message);
+    }
+  }
+
+  useEffect(() => {
+    getQuotes();
+  }, [])
+
+  return { quotes, loading, error }
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { IQuotesList } from '../types/types';
-import axios from 'axios';
+import React from 'react';
+import { useQuotes } from '../hooks/useQuotes';
 import Header from '../components/Header';
 import QuotesList from '../components/QuotesList';
 
 const Home: React.FC = () => {
-  const [quotes, setQuotes] = useState<IQuotesList[]>([]);
-
-  const API = 'https://api.quotable.io';
-
-  useEffect(() => {
-    getQuotes();
-  }, [])
-
-  async function getQuotes() {
-    try {
-      const response = await axios.get<IQuotesList[]>(`${API}/quotes`);
-      setQuotes(response.data.results);
-    } catch (e) {
-      console.log(e);
-    }
-  }
+  const {quotes, loading, error} = useQuotes();
 
   return (
     <>
       <Header/>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
       <QuotesList quotes={quotes}/>
     </>
   )
